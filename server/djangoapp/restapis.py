84d1c40fee3bc97a3455ac86fc2516b75a4d548e
david-geywitz <56725848+david-geywitz@users.noreply.github.com>
2024-03-29 16:14:51 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -27,6 +27,7 @@ def get_request(endpoint, **kwargs):
     except:
         # If any error occurs
         print("Network exception occurred")
+        raise
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
@@ -34,9 +35,10 @@ def analyze_review_sentiments(text):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
+    except as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+        raise
 
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
@@ -46,3 +48,4 @@ def post_review(data_dict):
         return response.json()
     except:
         print("Network exception occurred")
+        raise
