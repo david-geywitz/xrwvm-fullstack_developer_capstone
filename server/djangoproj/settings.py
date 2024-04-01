eb13329fa1d57d09baaa27be1ef1a47761a06f86
david-geywitz <56725848+david-geywitz@users.noreply.github.com>
2024-04-01 10:31:05 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -96,8 +96,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.User
-AttributeSimilarityValidator',
+'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
